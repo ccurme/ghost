f60414436bdaebf7f135d7559b8d352ee83c6fca
Chester Curme <chestercurme@microsoft.com>
2023-04-23 09:31:00 -0400

    refactor test

--- ghost/tests/unit_tests/test_app.py
+++ ghost/tests/unit_tests/test_app.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Callable
 import unittest
 from unittest.mock import MagicMock, patch
 
@@ -34,30 +34,60 @@ class TestApp(unittest.TestCase):
         cls.app = app.test_client()
         cls.ai_phone_number = "+18008675309"
 
-    @patch("app.initialize_agent")
-    @patch("app._make_twilio_client")
-    def test_agent_management(self, make_twilio_client, initialize_agent):
+    def _test_response(
+        self,
+        make_twilio_client: Callable,
+        incoming_number: str,
+        incoming_message: str,
+        reply: str,
+    ):
         mock_twilio_client = _make_mock_twilio_client()
         make_twilio_client.return_value = mock_twilio_client
-        incoming_number = "+18001234567"
         data = {
-            "Body": "Hey Ghost",
+            "Body": incoming_message,
             "From": incoming_number,
         }
-        reply = "Hi Marcos."
-        initialize_agent.return_value = _make_mock_agent(reply)
-        self.assertEqual(0, len(CONVERSATIONS))
+        reply = "Hello"
         response = self.app.post("/sms", data=data, content_type="multipart/form-data")
         self.assertEqual(200, response.status_code)
         self.assertEqual(MESSAGE_SID, response.text)
-        initialize_agent.assert_called_once()
         expected_call = {
             "body": reply,
             "from_": self.ai_phone_number,
             "to": incoming_number,
         }
         mock_twilio_client.messages.create.assert_called_once_with(**expected_call)
+
+    @patch("app.initialize_agent")
+    @patch("app._make_twilio_client")
+    def test_agent_management(self, make_twilio_client, initialize_agent):
+        self.assertEqual(0, len(CONVERSATIONS))
+        reply = "Hello"
+        initialize_agent.return_value = _make_mock_agent(reply)
+        self._test_response(
+            make_twilio_client,
+            "+18001234567",
+            "Hey Ghost",
+            reply,
+        )
+        initialize_agent.assert_called_once()
         self.assertEqual(1, len(CONVERSATIONS))
+        self._test_response(
+            make_twilio_client,
+            "+18001234567",
+            "How are you?",
+            reply,
+        )
+        initialize_agent.assert_called_once()
+        self.assertEqual(1, len(CONVERSATIONS))
+        self._test_response(
+            make_twilio_client,
+            "+18005555555",
+            "Hi",
+            reply,
+        )
+        self.assertEqual(2, initialize_agent.call_count)
+        self.assertEqual(2, len(CONVERSATIONS))
 
     @patch("app.initialize_agent")
     @patch("app._make_twilio_client")
