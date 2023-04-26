38e1ce783a486618c84aa96fe9a15b6bf8e20c7a
Chester Curme <chestercurme@microsoft.com>
2023-04-26 19:59:32 -0400

    factor out prompt for unsolicited message

--- ghost/unsolicited_message.py
+++ ghost/unsolicited_message.py
@@ -6,26 +6,30 @@ from langchain.agents.agent import AgentExecutor
 from utils import format_prompt_components_without_tools
 
 
-def generate_unsolicited_message(
-    ai_settings: dict, contact_settings: dict, agent: AgentExecutor
-) -> str:
-    """Generate AI message without message from user."""
-
-    ai_prefix, human_prefix, prefix, suffix = format_prompt_components_without_tools(
-        ai_settings, contact_settings
-    )
-
+def get_unsolicited_message_prompt(ai_prefix: str, human_prefix: str) -> str:
+    """Get prompt for unsolicited message."""
     inspirational_thought = f"""
     *{ai_prefix} then drew on their past experiences and relationships with {human_prefix} and continued the conversation*
 
     {ai_prefix}:
     """
-    inspirational_thought = dedent(inspirational_thought)
 
-    prompt = "\n".join([prefix, suffix, inspirational_thought]).format(
+    return dedent(inspirational_thought)
+
+
+def generate_unsolicited_message(
+    prompt: str, agent: AgentExecutor, ai_settings: dict, contact_settings: dict, temperature: int = 0,
+) -> str:
+    """Generate AI message without message from user."""
+
+    _, _, prefix, suffix = format_prompt_components_without_tools(
+        ai_settings, contact_settings
+    )
+
+    prompt = "\n".join([prefix, suffix, prompt]).format(
         chat_history=agent.memory.buffer
     )
-    llm = OpenAI(temperature=0)
+    llm = OpenAI(temperature=temperature)
     message = llm(prompt)
 
     agent.memory.chat_memory.add_ai_message(message)
