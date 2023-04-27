7d6a9b8ce29758c3f3559915745d6dcad174767b
Chester Curme <chestercurme@microsoft.com>
2023-04-26 21:28:13 -0400

    refactor app and add endpoint for unsolicited messages

--- ghost/app.py
+++ ghost/app.py
@@ -4,6 +4,7 @@ from flask import Flask, request
 from twilio.rest import Client
 
 from agent_utils import initialize_agent
+from unsolicited_message import generate_unsolicited_message
 from utils import load_settings
 
 app = Flask(__name__)
@@ -17,30 +18,80 @@ def _make_twilio_client():
     return Client(account_sid, auth_token)
 
 
-@app.route("/sms", methods=["POST"])
-def sms():
-    twilio_client = _make_twilio_client()
-    incoming_message = request.values["Body"]
-    chat_partner_phone_number = request.values["From"]
-    ai_settings, contacts = load_settings()
-    number_to_contact = {contact.pop("phone_number"): contact for contact in contacts}
-    if chat_partner_phone_number not in number_to_contact:
-        return "Unknown number."
+def _validate_number_and_get_agent(
+    chat_partner_phone_number: str, ai_settings: dict, contacts: dict
+):
+    """Validate phone number for chat partner and retrieve or initialize agent."""
+    if chat_partner_phone_number not in contacts:
+        return None
     else:
         if chat_partner_phone_number in AGENT_CACHE:
             agent_executor = AGENT_CACHE[chat_partner_phone_number]
         else:
             agent_executor = initialize_agent(
-                ai_settings, number_to_contact[chat_partner_phone_number]
+                ai_settings, contacts[chat_partner_phone_number]
             )
             agent_executor.agent.llm_chain.verbose = VERBOSE_PROMPT
             AGENT_CACHE[chat_partner_phone_number] = agent_executor
+
+        return agent_executor
+
+
+@app.route("/sms", methods=["POST"])
+def sms():
+    """Receive message and send response."""
+    twilio_client = _make_twilio_client()
+    incoming_message = request.values["Body"]
+    chat_partner_phone_number = request.values["From"]
+    ai_settings, contacts = load_settings()
+    contacts = {
+        contact.pop("phone_number"): contact for contact in contacts
+    }  # key contacts by phone number
+    agent_executor = _validate_number_and_get_agent(
+        chat_partner_phone_number, ai_settings, contacts
+    )
+    if agent_executor is None:
+        return "Unknown number."
+    else:
         response = agent_executor.run(incoming_message)
 
-        message = twilio_client.messages.create(
+        twliio_message = twilio_client.messages.create(
             body=response,
             from_=ai_settings["ai_phone_number"],
             to=chat_partner_phone_number,
         )
 
-        return message.sid
+        return twliio_message.sid
+
+
+@app.route("/unsolicited_message", methods=["POST"])
+def unsolicited_message():
+    """Send message."""
+    twilio_client = _make_twilio_client()
+    prompt = request.values["prompt"]
+    chat_partner_phone_number = request.values["to"]
+
+    ai_settings, contacts = load_settings()
+    contacts = {contact.pop("phone_number"): contact for contact in contacts}
+    agent_executor = _validate_number_and_get_agent(
+        chat_partner_phone_number, ai_settings, contacts
+    )
+    if agent_executor is None:
+        return "Unknown number."
+    else:
+        contact_settings = contacts[chat_partner_phone_number]
+        message = generate_unsolicited_message(
+            prompt,
+            agent_executor,
+            ai_settings,
+            contact_settings,
+            temperature=0.7,
+        )
+
+        twliio_message = twilio_client.messages.create(
+            body=message,
+            from_=ai_settings["ai_phone_number"],
+            to=chat_partner_phone_number,
+        )
+
+    return twliio_message.sid
