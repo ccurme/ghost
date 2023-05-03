2ff4ef83cfd0612c405d3224185fc503feb18b8c
Chester Curme <chestercurme@microsoft.com>
2023-05-03 15:12:48 -0400

    add function to fetch from sqlite and test

---
+++ ghost/fine_tuning/iphone_sqlite.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+import pandas as pd
+
+
+def pull_messages_for_contact_number(
+    connection: sqlite3.Connection, contact_number: str
+) -> pd.DataFrame:
+    """Pull text messages from iPhone sqlite backup for a particular phone number."""
+    query = """
+    SELECT
+        datetime (message.date / 1000000000 + strftime ("%s", "2001-01-01"), "unixepoch", "localtime") AS message_datetime,
+        message.text,
+        message.is_from_me,
+        chat.chat_identifier
+    FROM
+        chat
+        JOIN chat_message_join ON chat."ROWID" = chat_message_join.chat_id
+        JOIN message ON chat_message_join.message_id = message."ROWID"
+    WHERE message.text IS NOT NULL
+        AND chat.chat_identifier = ?
+    ORDER BY
+        chat_identifier ASC,
+        message_date ASC
+    """
+    df = pd.read_sql(query, connection, params=(contact_number,))
+    df = df[
+        (df["text"] != "Laughed at an image") & (df["text"] != "Loved an image")
+    ].reset_index(drop=True)
+
+    return df
